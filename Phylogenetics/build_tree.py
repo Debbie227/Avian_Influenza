7d5323ce656b63a6fb2c5feeba63a6daa2a8540d
Debra <lildebbie227@yahoo.com>
2025-06-07 09:46:24 -0600

    Changed build_tree.py code to a function Added file choosing capability Added description

--- Phylogenetics/build_tree.py
+++ Phylogenetics/build_tree.py
@@ -1,42 +1,77 @@
 #!/usr/bin/env python3
+import os
 
 from Bio import Phylo
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, DistanceMatrix
 from Bio import AlignIO
 from Bio.Phylo.TreeConstruction import DistanceCalculator
 import matplotlib.pyplot as plt
+import tkinter as tk
+
+from Protein_Analysis.amino_acid_compare import file_selector
 
 """
 File name: build_tree.py
-Author: Janessa Reed
+Author: Janessa Reed, Debra Pacheco
 Created: 02/04/25
-Version: 1.0
+Version: 1.2
 Description:
     
 
 License: MIT License
 """
 
-# Load the alignment
-alignment = AlignIO.read("H5_Aligned_Official (3).fasta", "fasta")
 
-# Compute pairwise distances
-calculator = DistanceCalculator("identity")
-dm = calculator.get_distance(alignment)
+def phyo_tree():
 
-# Construct a UPGMA tree
-constructor = DistanceTreeConstructor()
-tree = constructor.nj(dm)  # Neighbor-Joining method
+    """ When a FASTA formatted MSA file is selected via file selection, this will print to screen a distance based
+    phylogenetic tree.
 
-# Save the tree in Newick format
-#Phylo.write(tree, "H5_tree_upgma.nwk", "newick")
+    Parameters:
+    None
 
-# Display ASCII tree
-print("\nUPGMA Phylogenetic Tree:\n")
-Phylo.draw_ascii(tree)
+    Returns:
+    None
 
-# Graphical visualization
-plt.figure(figsize=(10, 8))
-Phylo.draw(tree, do_show=False)
-#plt.savefig("H5_tree_upgma.png")
-#print("\nTree saved as H5_tree_upgma.png")
+    """
+
+    try:
+        file_path = file_selector()
+    except tk.TclError:
+        file_path = input("Please enter file path.\n")
+
+    if os.path.exists(file_path):
+        print("Generating Distance tree.\n")
+    else:
+        print("File not found.")
+        exit()
+
+    # Load the alignment
+    alignment = AlignIO.read(file_path, "fasta")
+
+    # Compute pairwise distances
+    calculator = DistanceCalculator("identity")
+    dm = calculator.get_distance(alignment)
+
+    # Construct a UPGMA tree
+    constructor = DistanceTreeConstructor()
+    tree = constructor.nj(dm)  # Neighbor-Joining method
+
+    # Save the tree in Newick format
+    # Phylo.write(tree, "H5_tree_upgma.nwk", "newick")
+
+    # Display ASCII tree
+    print("\nUPGMA Phylogenetic Tree:\n")
+    Phylo.draw_ascii(tree)
+
+    # Graphical visualization
+    plt.figure(figsize=(10, 8))
+    Phylo.draw(tree, do_show=False)
+    # plt.savefig("H5_tree_upgma.png")
+    # print("\nTree saved as H5_tree_upgma.png")
+
+    return None
+
+
+if __name__ == "__main__":
+    phyo_tree()
